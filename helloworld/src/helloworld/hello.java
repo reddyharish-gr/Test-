375e793fd7c9bb91c34005edd0b8a07f9f287c48
reddy <reddy@DESKTOP-DK9E82H>
2020-06-30 00:11:19 +0530

    first commit

---
+++ helloworld/src/helloworld/hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author reddy
+ *
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+	System.out.println("sample file");
+
+	}
+
+}
